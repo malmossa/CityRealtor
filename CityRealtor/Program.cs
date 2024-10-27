23d409dc53ddd33bae083eccafd2045a16cdb61a
Mansor Almossa <malmossa@gmail.com>
2024-10-27 12:55:28 -0700

    completed

--- CityRealtor/Program.cs
+++ CityRealtor/Program.cs
@@ -1,9 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
+
 var app = builder.Build();
+app.UseStaticFiles();
 
 app.MapRazorPages();
+
 app.Run();
 
 
